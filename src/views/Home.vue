0db28a88e3dc1dd222d71e4e928ff0b8d77b971e
captain-tab <d491405879@gmail.com>
2021-03-31 23:05:06 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ greeting }}大头兄弟</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Test extends Vue {
+  name = "Test"
+  private greeting = "hello"
+}
+</script>
